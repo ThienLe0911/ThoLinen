735c7f3199bf34b312c6b0871aacd141cb3b7bdd
thien <lehollo09112000@gmail.com>
2024-09-28 23:42:26 +0700

    product page

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,7 +1,10 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Header from "../common/HeaderComponent/Header";
 import Footer from "../common/Footer/Footer";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import { faCaretUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import './product.css'
 
 function Product() {
@@ -36,7 +39,8 @@ function Product() {
   //     value: 5,
   //   },
   // ];
-  // const [sortSelect, setSortSelect] = useState(0);
+  const [sortSelect, setSortSelect] = useState(0);
+  const [showSort, setShowSort] = useState(false);
   // const [products, setProducts] = useState([]);
   return (
     <div className="product">
@@ -44,10 +48,42 @@ function Product() {
       <div className="product-wrap flex flex-col py-20 mt-20 items-center justify-center">
         <div className="product__title container_custom flex justify-between">
           <h2>SHOP</h2>
-          <div className="sorting p-5 flex justify-between">
-            <span>
-              helo
-            </span>
+          <div className="sorting p-2 flex justify-between bg-[#f7f7f7] h-fit relative">
+            <p onClick={() => { setShowSort(!showSort) }}>
+              Default sorting
+            </p>
+            {
+              showSort == true ? <FontAwesomeIcon
+                icon={faCaretUp}
+                className="fa-fw text-[40px]"
+              /> : <FontAwesomeIcon
+                icon={faCaretDown}
+                className="fa-fw text-[40px]"
+              />
+            }
+
+            <div className={(!showSort && 'hidden') + " absolute top-full w-full bg-[#f7f7f7] left-0 z-10"}>
+              <ul className="flex flex-col w-full">
+                <li onClick={() => setSortSelect(0)} className={(sortSelect == 0 ? 'bg-[#a7a6a6]' : '') + " w-full p-2"}>
+                  <span>Default sorting</span>
+                </li>
+                <li onClick={() => setSortSelect(1)} className={(sortSelect == 1 ? 'bg-[#a7a6a6]' : '') + " w-full p-2"}>
+                  <span>Sort by popularity</span>
+                </li>
+                <li onClick={() => setSortSelect(2)} className={(sortSelect == 2 ? 'bg-[#a7a6a6]' : '') + " w-full p-2"}>
+                  <span>Sort by average rating</span>
+                </li>
+                <li onClick={() => setSortSelect(3)} className={(sortSelect == 3 ? 'bg-[#a7a6a6]' : '') + " w-full p-2"}>
+                  <span>Sort by latest</span>
+                </li>
+                <li onClick={() => setSortSelect(4)} className={(sortSelect == 4 ? 'bg-[#a7a6a6]' : '') + " w-full p-2"}>
+                  <span>Sort by price: low to high</span>
+                </li>
+                <li onClick={() => setSortSelect(5)} className={(sortSelect == 5 ? 'bg-[#a7a6a6]' : '') + " w-full p-2"}>
+                  <span>Sort by price: high to low</span>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
         <div className="product_list container_custom flex flex-wrap justify-between">
